2e65ef21efe7c1663d4af8fd415ec848b20a1cde
bjowi <bjowi@users.noreply.github.com>
2024-12-07 14:09:05 +0100

    Change build tool, rearrange files

--- src/weather/options.py
+++ src/weather/options.py
@@ -1,8 +1,10 @@
 import argparse
-import sys
+import os
 
 
 def get_options():
+    home = os.environ.get('HOME', '')
+
     parser = argparse.ArgumentParser(
         prog='weather',
         description='Fetch weather data',
@@ -15,6 +17,9 @@ def get_options():
     parser.add_argument('-s', '--station',
                         type=str, required=True,
                         help="ICAO airport code")
+    parser.add_argument('--cachedir',
+                        type=str, default=f'{home}/.cache',
+                        help="Directory for downloaded files")
     parser.add_argument('--age', type=int, default=604800,
                         help="Wait this long before fetching data again")
     return parser.parse_args()
